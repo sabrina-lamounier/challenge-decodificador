f6e7f1d4370ea482b62d26f4f16e6797bab47cd6
sabrina-lamounier <sabrinalfmoura@gmail.com>
2024-08-27 02:17:13 -0300

    1.3

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const menstextArea = document.querySelector(".output__copiar__text__area");
 const output = document.querySelector(".output");
 const outputPadrao = document.querySelector(".output__padrao");
 const outputCopiar = document.querySelector(".output__copiar");
+const root = document.querySelector(":root");
+const btSwitch = document.querySelector(".switch");
+
 
 let matrizCodigo = [["e" , "enter"] , ["i" , "imes"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
 
@@ -39,7 +42,27 @@ function btnCopiatexto() {
     const stringCopiada = menstextArea.value;
     textArea.value = stringCopiada;
     updateClipboard(menstextArea.value);
+    menstextArea.value = "";
 }
+
+function btnSwitch(){
+    root.classList.toggle('azul');
+    btSwitch.classList.toggle('invert');
+    altImg(".cabecalho__logo");
+    altImg(".cabecalho__logo__bg");
+    altImg(".output__padrao__imagem");   
+}
+
+function altImg (classe) {
+    const atr = document.querySelector(classe);
+    if (atr.src.includes('azul')) {
+        atr.src = atr.src.replace("_azul", "");
+    }
+    else {  
+        atr.src = atr.src.replace('.png', '_azul.png');
+    }
+}
+
 
 function updateClipboard(newClip) {
     navigator.clipboard.writeText(newClip).then(
